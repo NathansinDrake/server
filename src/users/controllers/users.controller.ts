de25d7258901852a0b6ebb880ab22ae07f87d1a6
NathanSinDrake <enrique.luna99359@gmail.com>
2023-05-05 17:41:05 -0500

    roles

--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -11,7 +11,9 @@ import { UsersService } from '../services/users.service';
 import { User } from '../entities/users.entity';
 import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { CreateUserDto } from '../dto/create-user.dto';
-
+import { Role } from 'src/jwt.config';
+import { Roles } from 'src/roles/roles.decorator';
+import { JwtAuthGuard } from 'src/roles/auth/jwt-auth.guard';
 
 @Controller('users')
 @ApiTags('Users')
@@ -20,14 +22,20 @@ export class UsersController {
 
 
   @Get('me')
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.User)
   @ApiOperation({summary: 'Traer usuario'})
   getProfile(@Request() req) {
     return req.user;
   }
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.Admin)
   @ApiOperation({summary: 'Crear un usuario'})
   async create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
+
+  
 }
